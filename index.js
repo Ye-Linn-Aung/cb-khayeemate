198cc9b20c26cdeebaea97cf9e439c1bdb407afb
Ye-Linn-Aung <78471455+Ye-Linn-Aung@users.noreply.github.com>
2021-03-02 22:59:58 +0630

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.get('/', function (req, res) {
 	
 
 // Facebook Webhook 
-app.get('/webhooks', function (req, res) {
-    if (req.query['hub.verify_token'] === 'VERIFY_TOKEN') {
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Invalid verify token');
