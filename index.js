1a5686d1a874b986ca0a46a25d92d42ba86c82ef
Ye-Linn-Aung <78471455+Ye-Linn-Aung@users.noreply.github.com>
2021-03-02 23:22:31 +0630

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,42 @@ app.get('/webhook', function (req, res) {
     } else {
         res.send('Invalid verify token');
     }
-}); 
+});  
+
+// handler receiving messages
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            if (!kittenMessage(event.sender.id, event.message.text)) {
+                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            }
+        } else if (event.postback) {
+            console.log("Postback received: " + JSON.stringify(event.postback));
+        }
+    }
+    res.sendStatus(200);
+});
+
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
 
 // send rich message with kitten
 function kittenMessage(recipientId, text) {
@@ -66,9 +101,3 @@ function kittenMessage(recipientId, text) {
     return false;
     
 }; 
-
-if (event.message && event.message.text) {
-    if (!kittenMessage(event.sender.id, event.message.text)) {
-        sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-    }
-}
