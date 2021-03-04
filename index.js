e33d2f9e8e3f8afc48a064bade187f46d8c21f33
Ye-Linn-Aung <78471455+Ye-Linn-Aung@users.noreply.github.com>
2021-03-04 23:44:37 +0630

    Update index.js

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text) {
+        if (event.message) {
             if (!kittenMessage(event.sender.id, event.message.text)) {
-                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+                sendMessage(event.sender.id);
             }
         } else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
