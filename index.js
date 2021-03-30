5f661d347b190c672649e302bdded00427b5cd2a
Ye-Linn-Aung <yelinnaung@neptunemm.com>
2021-03-30 23:59:55 +0630

    Update

--- index.js
+++ index.js
@@ -72,15 +72,8 @@ function sendMessage(recipientId, message) {
         json: {
             recipient: {id: recipientId},
             message: message,
-        },
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'GET',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        } 
-    },
+        }
+    }, 
     function(error, response, body) {
         if (error) {
             console.log('Error sending message: ', error);
